5a26630a8d8a783d8b1c8c9981b6be07a6696a5c
Kexxox <20253ds063@gmail.com>
2026-04-07 19:44:29 -0600

    Se finalizaron detalles para lograr que se corra el codigo, solo faltan detalles de visual

--- Tiendita-Local/src/main/java/module-info.java
+++ Tiendita-Local/src/main/java/module-info.java
@@ -2,7 +2,16 @@ module com.example.tienditalocal {
     requires javafx.controls;
     requires javafx.fxml;
 
+    exports com.example.tienditalocal to javafx.graphics;
+    exports com.example.tienditalocal.controllers;
+    exports com.example.tienditalocal.services;
+    exports com.example.tienditalocal.models;
+    exports com.example.tienditalocal.repositories;
 
-    opens com.example.tienditalocal to javafx.fxml;
-    exports com.example.tienditalocal;
+    opens com.example.tienditalocal to
+            javafx.fxml;
+    opens com.example.tienditalocal.controllers to
+            javafx.fxml;
+    opens com.example.tienditalocal.models to
+            javafx.base;
 }
