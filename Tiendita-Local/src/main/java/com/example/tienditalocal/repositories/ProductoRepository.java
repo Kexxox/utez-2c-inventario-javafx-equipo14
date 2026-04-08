436337d389208128cd2040b5cc88fad9603e100e
luis felipe <20253ds069@utez.edu.mx>
2026-04-07 19:28:08 -0600

    Se agrego repository solo falta models y terminaremos

--- Tiendita-Local/src/main/java/com/example/tienditalocal/repositories/ProductoRepository.java
+++ Tiendita-Local/src/main/java/com/example/tienditalocal/repositories/ProductoRepository.java
@@ -1,4 +1,40 @@
 package com.example.tienditalocal.repositories;
 
+import com.example.tienditalocal.models.Producto;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductoRepository {
+    private final String FOLDER_NAME = "Data";
+    private final String FILE_NAME = FOLDER_NAME + "/persons.csv";
+
+
+    public void saveAll(List<Producto> productos) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
+            for (Producto p : productos) {
+                writer.println(p.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Producto> loadAll() {
+        List<Producto> lista = new ArrayList<>();
+        File file = new File(FILE_NAME);
+        if (!file.exists()) return lista;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] d = line.split(",");
+                lista.add(new Producto(d[0], d[1], Double.parseDouble(d[2]), Integer.parseInt(d[3]), d[4]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
 }
